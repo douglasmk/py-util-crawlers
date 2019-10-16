6603444d8beb4c5c6a3371204185f40188140710
Douglas Martinello Karling <douglasmartinello@gmail.com>
2019-10-15 21:11:30 -0300

    Definição de timezone

--- cambio_crawler.py
+++ cambio_crawler.py
@@ -3,6 +3,7 @@ from FlexCambio import FlexCambioCrawler
 from send_email import SendEmail
 import csv
 import datetime
+from pytz import timezone
 
 # deboniCrawler = DeboniCambioCrawler()
 # deboniCrawler.crawl()
@@ -10,7 +11,7 @@ import datetime
 flexCrawler = FlexCambioCrawler()
 flexCrawler.crawl()
 
-dt = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+dt = datetime.datetime.now().astimezone(timezone('America/Sao_Paulo')).strftime("%d/%m/%Y %H:%M:%S")
 txtEmail = 'VALORES ATUALIZADOS EM '+dt
 # txtEmail += '\n--------------------------------------------------------------\n'
 # txtEmail += 'Deboni  '+deboniCrawler.getCotacaoFormatada()+'  https://debonicambio.com.br'
@@ -19,4 +20,4 @@ txtEmail += 'Flex  '+flexCrawler.getCotacaoFormatada()+'  https://www.flexcambio
 txtEmail += '\n--------------------------------------------------------------\n'
 print(txtEmail)
 email = SendEmail()
-email.send('Cotação Euro '+dt, txtEmail)
+# email.send('Cotação Euro '+dt, txtEmail)
