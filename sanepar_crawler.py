a8cbceb63f05df6027c8581a86093c79fb5ceecb
Douglas Martinello Karling <douglas.karling@sistemafiep.org.br>
2021-02-06 15:23:37 -0300

    Adicionado tratamento de erros ao recuperar dados do rodizio

--- sanepar_crawler.py
+++ sanepar_crawler.py
@@ -12,27 +12,32 @@ saneparNivel = SaneparNivelReservatorioCrawler()
 niveis = saneparNivel.crawl()
 
 dt = datetime.datetime.now().astimezone(timezone('America/Sao_Paulo')).strftime("%d/%m/%Y %H:%M:%S")
-txtEmail =  ' PRÓXIMAS INTERRUPÇÕES NO FORNECIMENTO DE ÁGUA\n'
-txtEmail += ' _____________________________________________________________________________\n'
-txtEmail += '|       INÍCIO        |      RETOMADA       |          NORMALIZAÇÃO           |\n'
-txtEmail += ' -----------------------------------------------------------------------------\n'
+txtEmail = ''
+try:
+    txtEmail += ' PRÓXIMAS INTERRUPÇÕES NO FORNECIMENTO DE ÁGUA\n'
+    txtEmail += ' _____________________________________________________________________________\n'
+    txtEmail += '|       INÍCIO        |      RETOMADA       |          NORMALIZAÇÃO           |\n'
+    txtEmail += ' -----------------------------------------------------------------------------\n'
 
-for rodizio in rodizios :
-    txtEmail += '| '+rodizio.getInicio()+' | '+rodizio.getRetomada()+' | '+rodizio.getNormalizacao()+' ('+rodizio.getPeriodo()+') | '+rodizio.getObservacao()
-    txtEmail += '\n -----------------------------------------------------------------------------\n'
+    for rodizio in rodizios :
+        txtEmail += '| '+rodizio.getInicio()+' | '+rodizio.getRetomada()+' | '+rodizio.getNormalizacao()+' ('+rodizio.getPeriodo()+') | '+rodizio.getObservacao()
+        txtEmail += '\n -----------------------------------------------------------------------------\n'
 
-if len(niveis) > 0 :
-    txtEmail += '\n\n ______________________________'
-    txtEmail += '\n| NÍVEL DOS RESERVATÓRIOS '
-    txtEmail += '\n ------------------------------'
-    txtEmail += '\n| '+saneparNivel.dataAtualizacao
-    txtEmail += '\n =============================='
-
-    for nivel in niveis :
-        txtEmail += '\n| '+nivel.getNome()+': '+nivel.getNivel()+' '
+    if len(niveis) > 0 :
+        txtEmail += '\n\n ______________________________'
+        txtEmail += '\n| NÍVEL DOS RESERVATÓRIOS '
         txtEmail += '\n ------------------------------'
+        txtEmail += '\n| '+saneparNivel.dataAtualizacao
+        txtEmail += '\n =============================='
 
-    txtEmail += '\n *Sistema de Abastecimento de Água Integrado de Curitiba'
+        for nivel in niveis :
+            txtEmail += '\n| '+nivel.getNome()+': '+nivel.getNivel()+' '
+            txtEmail += '\n ------------------------------'
+
+        txtEmail += '\n *Sistema de Abastecimento de Água Integrado de Curitiba'
+except:
+    txtEmail += '|                   FALHA AO RECUPERAR A TABELA DE RODIZIOS                   |\n'
+    txtEmail += ' -----------------------------------------------------------------------------\n'
 
 if len(rodizios) > 0 or len(niveis) > 0 :
     print(txtEmail)
